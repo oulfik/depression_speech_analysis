bd61c861c45216cafac37125df6d8388702e425f
oliver ulfik <oulfik@gmx.de>
2022-08-09 12:12:00 +0200

    added files

--- slicedAudio.py
+++ slicedAudio.py
@@ -2,25 +2,47 @@
 For that purpose the transcript csv files are used where timestamps and the participants utterances are documented."""
 
 
-import csv 
+import csv, os
 from pydub import AudioSegment
 #from pydub.playback import play
 
-excluded_sessions = [342, 394, 398, 460]
-start_session_num = 300
-end_session_num = 492
+"""For DAIC """
+# def readCSV(csv_file: str) -> list[int]:
+#     with open(csv_file) as csvFile:
+#         csv_reader = csv.reader(csvFile, delimiter='\t')
+#         line_count = 0
+#         timestamps = []
+#         for row in csv_reader:
+#             print(row)
+#             line_count += 1
+#             if line_count == 1:
+#                 continue
+#             if 'Participant' in row and '<synch>' not in row:
+#                 timestamps.extend(row[:2])
+                
+#     for i in range(0, len(timestamps)):
+#         timestamps[i] = int(float(timestamps[i]) * 1000)
+  
+#     return timestamps
 
-def readCSV(csv_file: str) -> list[int]:
+"""For extended DAIC"""
+def get_speech_timestamps(csv_file: str) -> list[int]:
     with open(csv_file) as csvFile:
-        csv_reader = csv.reader(csvFile, delimiter='\t')
+        csv_reader = csv.reader(csvFile, delimiter=',')
         line_count = 0
         timestamps = []
+        previous_row = []
         for row in csv_reader:
+            print(row)
             line_count += 1
             if line_count == 1:
                 continue
-            if 'Participant' in row and '<synch>' not in row:
-                timestamps.extend(row[:2])
+            if line_count == 2:
+                previous_row = row
+            if line_count >= 3:
+                if int(float(row[0])) > int(float(previous_row[0])):
+                    timestamps.extend(row[:2])
+                    previous_row = row
                 
     for i in range(0, len(timestamps)):
         timestamps[i] = int(float(timestamps[i]) * 1000)
@@ -55,13 +77,21 @@ def cutAudio(audioFileName: str, offset: float, fromBeginning=True, fromEnd=Fals
 
 
 if __name__ == "__main__":
-    for session_id in range(start_session_num, end_session_num+1):
-        if session_id not in excluded_sessions:
-            csv_filename = f'{session_id}_TRANSCRIPT.csv'
-            audiofile_name = f'{session_id}_AUDIO.wav'
-            if '443' in audiofile_name:
-                pass
-                # audiofile_name = f'{session_id}_AUDIO_SLICED.wav'
-                # cutAudio(audiofile_name, offset=30.0, fromBeginning=False, fromEnd=True)
-            audiofile_name = f'{session_id}_AUDIO_SLICED.wav'
-            cutAudio(audiofile_name, offset=30.0, fromBeginning=False, fromEnd=True)
+    for file in os.listdir('.'):
+        if '.csv' in file:
+            print(file)
+            x = get_speech_timestamps(file)
+            audio_file = file.replace("Transcript.csv", "AUDIO.wav")
+            segmentAudio(audio_file, x)
+     
+
+    # for session_id in range(start_session_num, end_session_num+1):
+    #     if session_id not in excluded_sessions:
+    #         csv_filename = f'{session_id}_TRANSCRIPT.csv'
+    #         audiofile_name = f'{session_id}_AUDIO.wav'
+    #         if '443' in audiofile_name:
+    #             pass
+    #             # audiofile_name = f'{session_id}_AUDIO_SLICED.wav'
+    #             # cutAudio(audiofile_name, offset=30.0, fromBeginning=False, fromEnd=True)
+    #         audiofile_name = f'{session_id}_AUDIO_SLICED.wav'
+    #         cutAudio(audiofile_name, offset=30.0, fromBeginning=False, fromEnd=True)
